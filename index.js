79c0f64d5eb95ddf5e4fce2e1426f9d836ffe882
Igor Mestechkin <Igor.mestechkin@gmail.com>
2022-07-13 15:52:24 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Your code goes here
+document.addEventListener('DOMContentLoaded', function () {
+    const change = document.querySelector('#text')
+    change.textContent = 'This is really cool!'
+})
